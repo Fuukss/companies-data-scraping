ac01a863645fd72ca2e1fd273e535748bd5861f8
Fuukss <rafal19fuchs@gmail.com>
2021-11-16 16:02:59 +0100

    added CategoryScraper class

--- category.py
+++ category.py
@@ -7,54 +7,53 @@ import random
 from bs4 import BeautifulSoup
 from requests import get
 from objects import MainCategory, SubCategory
+from dataclasses import dataclass
 
 
-URL_PANORAMA: str = 'https://panoramafirm.pl/budownictwo,b/branze.html'
-URL_PANORAMA_MAIN: str = 'https://panoramafirm.pl'
+@dataclass
+class CategoryScraper:
+    URL_PANORAMA: str = 'https://panoramafirm.pl/budownictwo,b/branze.html'
+    URL_PANORAMA_MAIN: str = 'https://panoramafirm.pl'
+    subcategory_list_of_objects = []
+    category_list_of_objects = []
 
-industry_page = get(f'{URL_PANORAMA}')
-bs_panorama_web_site = BeautifulSoup(industry_page.content, 'html.parser')
+    def __init__(self):
+        self.industry_page = get(f'{self.URL_PANORAMA}')
+        self.bs_panorama_web_site = BeautifulSoup(self.industry_page.content, 'html.parser')
 
-subcategory_list_of_objects = []
-category_list_of_objects = []
+    def get_list_of_categories(self) -> list:
+        """
+        Return list of instance of an objects - categories
+        :return: category_object_list: href(), __str__()
+        """
+        class_ = 'card-header bg-transparent border-0 py-1 cursor-pointer'
+        for categories in self.bs_panorama_web_site.find_all('div', class_):
+            category = categories.find('a', href=True)
+            category = category['href']
+            category_object = MainCategory(category)
+            self.category_list_of_objects.append(category_object)
+        return self.category_list_of_objects
 
+    def bs_subcategories_site_parse_html(self, categories: list) -> BeautifulSoup:
+        page_subcategory = get(f'{self.URL_PANORAMA_MAIN}{categories.href()}')
+        bs_panorama_sub = BeautifulSoup(page_subcategory.content, 'html.parser')
+        return bs_panorama_sub
 
-def get_list_of_categories() -> list:
-    """
-    Return list of instance of an objects - categories
-    :return: category_object_list: href(), __str__()
-    """
-    class_ = 'card-header bg-transparent border-0 py-1 cursor-pointer'
-    for categories in bs_panorama_web_site.find_all('div', class_):
-        category = categories.find('a', href=True)
-        category = category['href']
-        category_object = MainCategory(category)
-        category_list_of_objects.append(category_object)
-    return category_list_of_objects
+    def get_list_of_subcategories(self) -> list:
+        """
+        Return list of instance of an objects - subcategories
+        :return: subcategory_object_list: href(), __str__()
+        """
+        for categories in self.category_list_of_objects:
+            for subcategory in self.bs_subcategories_site_parse_html(categories)('li', class_='py-1'):
+                subcategory = subcategory.find('a', href=True)
+                subcategory = subcategory['href']
+                subcategory_object = SubCategory(categories, subcategory)
+                self.subcategory_list_of_objects.append(subcategory_object)
+        return self.subcategory_list_of_objects
 
-
-def bs_subcategories_site_parse_html(categories: list) -> BeautifulSoup:
-    page_subcategory = get(f'{URL_PANORAMA_MAIN}{categories.href()}')
-    bs_panorama_sub = BeautifulSoup(page_subcategory.content, 'html.parser')
-    return bs_panorama_sub
-
-
-def get_list_of_subcategories() -> list:
-    """
-    Return list of instance of an objects - subcategories
-    :return: subcategory_object_list: href(), __str__()
-    """
-    for categories in category_list_of_objects:
-        for subcategory in bs_subcategories_site_parse_html(categories)('li', class_='py-1'):
-            subcategory = subcategory.find('a', href=True)
-            subcategory = subcategory['href']
-            subcategory_object = SubCategory(categories, subcategory)
-            subcategory_list_of_objects.append(subcategory_object)
-    return subcategory_list_of_objects
-
-
-def get_random_count_of_subcategories_objects(item_count: int) -> list:
-    random_list_of_objects = random.choices(subcategory_list_of_objects, k=item_count)
-    random_list_of_elements = [[x.href(), str(x), str(x.return_category_name())]
-                               for x in random_list_of_objects]
-    return random_list_of_elements
+    def get_random_count_of_subcategories_objects(self, item_count: int) -> list:
+        random_list_of_objects = random.choices(self.subcategory_list_of_objects, k=item_count)
+        random_list_of_elements = [[x.href(), str(x), str(x.return_category_name())]
+                                   for x in random_list_of_objects]
+        return random_list_of_elements
